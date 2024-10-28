46bb12ea6dac902260f05f1b83264f7a2fe78806
NSQY <34185288+NSQY@users.noreply.github.com>
2024-10-28 23:04:17 +1030

    push enums

---
+++ vsmetrics/enums/colorspace.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class ColourSpace(Enum):
+    INPUT = 0
+    GAMMA = 1
+    LINEAR = 2
+    PERCEPTUAL = 3
+    OKLAB = 4
---
+++ vsmetrics/enums/visualizediffs.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class ColormapTypes(Enum):
+    AUTUMN = 0
+    BONE = 1
+    JET = 2
+    WINTER = 3
+    RAINBOW = 4
+    OCEAN = 5
+    SUMMER = 6
+    SPRING = 7
+    COOL = 8
+    HSV = 9
+    PINK = 10
+    HOT = 11
+    PARULA = 12
+    MAGMA = 13
+    INFERNO = 14
+    PLASMA = 15
+    VIRIDIS = 16
+    CIVIDIS = 17
+    TWILIGHT = 18
+    TWILIGHT_SHIFTED = 19
+    TURBO = 20
+    DEEPGREEN = 21
